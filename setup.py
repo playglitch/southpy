dd2b95c5b4004f6d2165035e0771a26851bcbe59
doamatto (Ronchetto, Matt) <hello@doamatto.xyz>
2020-07-17 08:58:00 -0700

    Added setup files for PIP

---
+++ setup.py
@@ -0,0 +1,34 @@
+import pathlib
+from setuptools import setup
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+setup(
+    name= "southpy",
+    version= "v0.1.0",
+    description= "Python Wrapper for Southpine's Player API",
+    license= "BSD-3-License",
+    long_description= README,
+    long_description_content_type= "text/markdown",
+    author= "doamatto",
+    maintainer= "doamatto",
+    maintainer_email= "hello@doamatto.xyz",
+    package_data= { "southpy": ["py.typed"] },
+    packages= { "southpy" },
+    url= "https://github.com/doamatto/southpy",
+    install_requires=["requests", "aiohttp"],
+    keywords= ["api", "southpine", "southpy"]
+    classifiers= [
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved",
+    ],
+    zip_safe= False,
+    python_requires=">= 3.6",
+    project_urls={
+        "Bug Reports": "https://github.com/doamatto/southpy/issues",
+        "Source": "https://github.com/doamatto/southpy"
+    }
+)
