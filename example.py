e32de7496dcfd1c4d438872473ee04d365cd3ed2
doamatto (Ronchetto, Matt) <hello@doamatto.xyz>
2020-07-17 09:00:52 -0700

    Added example usage

---
+++ example.py
@@ -0,0 +1,7 @@
+from southpy import South
+from pprint import pprint
+
+south = South()
+
+pprint(south.mini_profile("doamatto"))
+pprint(south.lobby(121, "5up3r53cr3tAuthT0k3n")) # Note: Not a real Auth Token
